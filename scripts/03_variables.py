3ebab5c826ae242854d2ceeb1d2181475b433eaa
Baraa Khatib Salkini <69188047+DataWithBaraa@users.noreply.github.com>
2025-05-07 15:57:27 +0200

    Create 03_variables.py

---
+++ scripts/03_variables.py
@@ -0,0 +1,8 @@
+#===================================================================
+# PRINT()
+# ---------------------------------------
+# The `print()` built-in Python function is used to display text
+# on the screen. It’s your main way to *communicate* with users
+# and check what your code is doing.
+# You’ll use it in almost every Python program!
+#===================================================================
