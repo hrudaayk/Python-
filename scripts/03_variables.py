1bd5f54ac36f54d0335e9ea2830a34afe4a5c4a2
Baraa Khatib Salkini <69188047+DataWithBaraa@users.noreply.github.com>
2025-05-07 17:54:07 +0200

    Update 03_variables.py

--- scripts/03_variables.py
+++ scripts/03_variables.py
@@ -1,8 +1,51 @@
-#===================================================================
-# PRINT()
+# ================================================================================
+# VARIABLES 
+# ----------------------------------------
+# Variables act as named containers for storing data in Python.
+# You give a variable a value using the assignment operator `=`, then
+# you can reuse that name throughout your code. We’ll also use
+# the `print()` function to display both literals and variables.
+# ================================================================================
+
 # ---------------------------------------
-# The `print()` built-in Python function is used to display text
-# on the screen. It’s your main way to *communicate* with users
-# and check what your code is doing.
-# You’ll use it in almost every Python program!
-#===================================================================
+# Without Variables
+# ---------------------------------------
+# Here we print fixed text directly using print().
+print("My name is Baraa")
+print("Baraa is learning Python")
+print("Baraa wants to become Python expert")
+
+
+# ---------------------------------------
+# Single Variable
+# ---------------------------------------
+# Store your name in a variable and reuse it in print().
+name = "Baraa"
+print("My name is", name)
+print(name, "is learning Python")
+print(name, "wants to become Python expert")
+
+
+# ---------------------------------------
+# Multiple Variables
+# ---------------------------------------
+# Now we’ll keep both your name and the language in variables.
+name     = "Baraa"
+language = "python"
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "wants to become", language, "expert")
+
+# ---------------------------------------
+# Python Challenge
+# ---------------------------------------
+# Print the following three lines:
+#     info@datawithbaraa.com
+#     support@datawithbaraa.com
+#     www.datawithbaraa.com
+# Use a variable for the base domain to make it dynamic!
+
+domain = "datawithbaraa.com"
+print("info@" + domain)
+print("support@" + domain)
+print("www." + domain)
