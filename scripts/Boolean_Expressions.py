259f02b13fa63e9950ed5fcec79078d92928a847
hrudaayk <155105149+hrudaayk@users.noreply.github.com>
2025-06-24 22:42:09 +0530

    Create Boolean_Expressions.py

---
+++ scripts/Boolean_Expressions.py
@@ -0,0 +1,17 @@
+Name = "Hrudaay"
+Mobile = 99
+Email = "hrudaay007@gmail.com"
+
+#Allows Registraion
+#if any field is Filled 
+print(any([Name,Mobile,Email]))
+
+#Allows Registraion
+#if any field is Filled 
+print(all([Name,Mobile,Email]))
+#Checks the data type 
+print(isinstance(Mobile, int))
+#checks the lastletter 
+print("Hello".endswith("o"))
+#checks the firstletter 
+print("Hello".startswith("o"))
